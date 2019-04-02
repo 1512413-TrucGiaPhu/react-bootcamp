d99b74dd77cca1697d25c268a895295d23f83c61
Trực Gia Phú - 1512413 <1512413@student.hcmus.edu.vn>
2019-04-02 17:08:53 +0700

    add Header component

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './components/App';
+import { Router, Switch, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
+import Jokes from './components/Jokes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router history={createBrowserHistory()}>
+        <Switch>
+            <Route exact path='/' component={App} />
+            <Route path='/jokes' component={Jokes} />
+        </Switch>
+    </Router>,
+    document.getElementById('root'));
