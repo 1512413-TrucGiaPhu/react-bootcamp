c5feebe79af86aa540b5e537a481c335cb4b7767
Trực Gia Phú - 1512413 <1512413@student.hcmus.edu.vn>
2019-04-07 18:40:39 +0700

    add music-master as a submodule

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -5,12 +5,14 @@ import { Router, Switch, Route } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory'
 import Jokes from './components/Jokes';
 import Header from './components/Header'
+import MusicMaster from './projects/music-master';
 
 ReactDOM.render(
     <Router history={createBrowserHistory()}>
         <Switch>
             <Route exact path='/' render={()=> <Header><App/></Header>} />
             <Route path='/jokes' render={()=> <Header><Jokes/></Header>} />
+            <Route path='/music-master' render={()=> <Header><MusicMaster/></Header>} />
         </Switch>
     </Router>,
     document.getElementById('root'));
